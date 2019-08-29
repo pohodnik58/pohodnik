1582394f783c763b30bb8599a6f3a6c0ac87f201
pohodnik58 <pohodnik58@gmail.com>
2019-08-29 21:25:27 +0300

    simple router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import './index.css';
 import style from './style.m.less'
-const x = [1,2,3,4];
-class App extends React.Component {
-    render() {
-        return <h1 className={style.h1}>Pohodnik!</h1>
-    }
-}
+import App from './App';
+
 ReactDOM.hydrate(<App />, document.getElementById("root"));
 
