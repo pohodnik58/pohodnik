9f398d8604ea83512d39769ba20d3f01575277b3
Elena <ugoljok91@gmail.com>
2020-03-26 23:23:27 +0300

    много всего
    создали файл со  списком тестов и файл с отделными тестами

---
+++ src/tests/firstTests.test.ui.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+import  {getBtnText, getLoginInput, getPassInput} from './tests';
+
+let page;
+let browser;
+const width = 1920;
+const height = 1080;
+
+
+beforeAll(async() => {
+    browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 0,
+        args: [`--window-size=${width},${height}`]
+    });
+    page = await browser.newPage();
+    await page.setViewport({width, height});
+    await page.goto('http://localhost:8080/login');
+});
+
+describe ('Страница авторизации', () => {
+    test('Ожидается наличие кнопки Войти', () => getBtnText(page));
+    test('Ожидается наличие поля Логин', () => getLoginInput(page));
+    test('Ожидается наличие поля Пароль', () => getPassInput(page));
+})
+
+
+afterAll(async() => {
+    await browser.close();
+});
